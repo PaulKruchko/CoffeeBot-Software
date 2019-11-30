222149b35c3f37d63748fbc43f3b19c8553379a8
spuzzuol <58037314+spuzzuol@users.noreply.github.com>
2019-11-29 21:30:05 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,28 @@
-import pyb
+import pyb, time
 from pyb import Pin
 
+#function to debounce pin
 def wait_pin_change(pin):
-   # wait for pin to change value
-   # it needs to be stable for a continuous 20ms
-   cur_value = pin.value()
-   active = 0
-   while active < 20:
-      if pin.value() != cur_value:
-         active += 1
-      else:
-         active = 0
-      pyb.delay(1)
+# wait for pin to change value
+# it needs to be stable for a continuous 20ms
+	cur_value = pin.value()
+        active = 0
+   	while active < 20:
+        	if pin.value() != cur_value:
+         		active += 1
+      		else:
+         		active = 0
+      		pyb.delay(1)
 
-leftWhisker = Pin('X19', Pin.IN)
-rightWhisker = Pin('X20', Pin.IN)
+#define pins for whiskers
+leftWhisker = Pin('X19', Pin.IN, Pin.PULL_UP)
+rightWhisker = Pin('X20', Pin.IN, Pin.PULL_UP)
 
+#loop for checking pin values
 while True:
-    wait_pin_change(leftWhisker)
-    print('left')
-    print(leftWhisker.value())
+	print('entered loop')
+	print(wait_pin_change(leftWhisker).value())
+	if wait_pin_change(leftWhisker).value() == 0 or wait_pin_change(rightWhisker).value() == 0:
+		print('T')
+	else:
+		print('F')
