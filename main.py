7c5ea147bd0ab5ba49d5b5b08a702fe9ebae6afe
spuzzuol <58037314+spuzzuol@users.noreply.github.com>
2019-11-30 13:59:09 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
-import pyb, time
+import encoderLib
 from pyb import Pin
 
+# Code section for encoder test
+last = 0
+
+# Initializes the library with pin on X1
+e = encoderLib.encoder('X1')
+
+while True:  # Infinite loop
+    value = e.getValue()  # Get rotary encoder value
+    if value != last:  # If there is a new value do
+        last = value
+        print(value)  # In this case it prints the value
+
+# Code section for whisker test
 #function to debounce pin
 def wait_pin_change(pin):
 # wait for pin to change value
