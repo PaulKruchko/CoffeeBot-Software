522bbf64d3f2dc13b4f967760955e15e4b84b945
spuzzuol <58037314+spuzzuol@users.noreply.github.com>
2019-12-02 09:28:54 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,27 @@
 import pyb
-from Switch import Switch 
+import encoderLib #for encoder
+from Switch import Switch #for switch debounce
 
 #define Switch class attributes for whiskers
 leftWhisker = Switch('X19', 50)
 rightWhisker = Switch('X20', 50)
+# Initializes the library with pin CLK on X1
+e = encoderLib.encoder('X1')
+#will return current distance traveled in cm as it moves
+last = 0
 
+while True:
+#encoder code
+#right now it just prints the distance traveled in cm
+	value = e.getValue()
+    	if value != last:
+        	last = value
+        	print(value)
 #loop for checking whisker values
 #right now will just print T for true if either goes low
-while True:
-	if leftWhisker.getValue() == 0:
+    	elif leftWhisker.getValue() == 0:
 		leftWhisker.wait_pin_change()
 		print('T1')
 	elif rightWhisker.getValue() == 0:
 		rightWhisker.wait_pin_change()
 		print('T2')
-
-#ignore this for now its my non-working encoder example
-#last = 0
-
-# Initializes the library with pin on X1
-#e = encoderLib.encoder('X1')
-
-#while True:  # Infinite loop
-#    value = e.getValue()  # Get rotary encoder value
-#    if value != last:  # If there is a new value do
-#        last = value
-#        print(value)  # In this case it prints the value
